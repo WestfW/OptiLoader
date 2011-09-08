6f6d15b07edbcb32ba8e46e260ff3eaff6a7f70c
WestfW <westfw@gmail.com>
2011-09-08 08:36:51 -0700

    First commit to github.   Adds copyright and license (MIT)   Adds probe of RESET to check cable connection.   Adds check of "enter program mode" results.

---
+++ optiLoader.h
@@ -0,0 +1,25 @@
+#include <WProgram.h>
+
+typedef struct image {
+    char image_name[30];	       /* Ie "optiboot_diecimila.hex" */
+    char image_chipname[12];	       /* ie "atmega168" */
+    uint16_t image_chipsig;	       /* Low two bytes of signature */
+    byte image_progfuses[5];	       /* fuses to set during programming */
+    byte image_normfuses[5];	       /* fuses to set after programming */
+    byte image_pagesize;	       /* page size for flash programming */
+    char image_hexcode[1500];	       /* intel hex format image (text) */
+} image_t;
+
+typedef struct alias {
+  char image_chipname[12];
+  uint16_t image_chipsig;
+  image_t * alias_image;
+} alias_t;
+
+#define FUSE_PROT 0			/* memory protection */
+#define FUSE_LOW 1			/* Low fuse */
+#define FUSE_HIGH 2			/* High fuse */
+#define FUSE_EXT 3			/* Extended fuse */
+
+// Forward decl
+extern image_t PROGMEM image_328, image_328p, image_168, image_8;
